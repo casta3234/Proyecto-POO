ae059511c87fc2ec1e06f835c8b0e51a91654c56
Estudiante <Estudiante@10.203.3.41>
2017-10-17 16:43:39 -0500

    Ejercicio en clase 24/08/2017-eed1

---
+++ src/proyecto/Bloque.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Bloque extends JLabel {
+    
+private int x;
+private int y;
+private int ancho;
+private int largo;
+private String url;
+        
+    public Bloque(String url) {
+        super();
+        this.x = (int) Math.random() * 700 + 300;
+        this.y = (int) Math.random() * 490 + 15;
+    }
+ 
+}
